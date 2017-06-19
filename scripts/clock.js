460f5249e772fa612ce79e1576c2331a53f99cb7
Phillip Malboeuf <phil@boeuf.coffee>
2017-06-19 18:07:23 -0400

    Fixed angles

--- scripts/clock.js
+++ scripts/clock.js
@@ -9,7 +9,12 @@ export class Clock extends React.Component {
 	constructor(props) {
 		super(props)
 
-		let now = new Date()
+		this.state = {
+			radius: 300
+		}
+	}
+
+	resetClock() {
 		let sunrise = new Date()
 		sunrise.setHours(5)
 		sunrise.setMinutes(30)
@@ -27,17 +32,18 @@ export class Clock extends React.Component {
 		nextSunrise.setSeconds(0)
 		nextSunrise.setMilliseconds(0)
 
-		this.state = {
-			radius: 300,
+		this.setState({
 			now: Date.now(),
 			sunrise: sunrise.getTime(),
 			sunset: sunset.getTime(),
-			nextSunrise: nextSunrise.getTime()
-		}
-
-		this.state.day = this.state.nextSunrise - this.state.sunrise
+			nextSunrise: nextSunrise.getTime(),
+			day: nextSunrise.getTime() - sunrise.getTime()
+		})
 	}
 
+	componentWillMount() {
+		this.resetClock()
+	}
 
 	componentDidMount() {
 		this.interval = window.setInterval(this.updateNow.bind(this), 1000)
@@ -48,9 +54,14 @@ export class Clock extends React.Component {
 	}
 
 	updateNow() {
-		this.setState({
-			now: Date.now()
-		})
+		let now = Date.now()
+
+		if (now - this.state.sunrise > this.state.day) {
+			this.resetClock()
+		} else {
+			this.setState({now: now})
+		}
+			
 	}
 
 
@@ -67,11 +78,12 @@ export class Clock extends React.Component {
 		let now = (this.state.now - this.state.sunrise) / this.state.day
 		let sunrise = (this.state.sunrise - this.state.sunrise) / this.state.day
 		let sunset = (this.state.sunset - this.state.sunrise) / this.state.day
+		let nextSunrise = (this.state.nextSunrise - this.state.sunrise) / this.state.day
 
-		let rotateX = (value, percentage)=> center + ((radius + value) * Math.cos((percentage + 0.25)*2*Math.PI))
-		let rotateY = (value, percentage)=> center + ((radius + value) * Math.sin((percentage + 0.25)*2*Math.PI))
+		let rotateX = (value, percentage)=> center + ((radius + value) * Math.cos((percentage + 0.5)*2*Math.PI))
+		let rotateY = (value, percentage)=> center + ((radius + value) * Math.sin((percentage + 0.5)*2*Math.PI))
 
-		let time = now >= sunset ? `N${((now-sunset)/(sunrise-sunset)*-100).toFixed(1)}` : `D${((now-sunrise)/(sunset-sunrise)*100).toFixed(1)}`
+		let time = now >= sunset ? `N${((now-sunset)/(nextSunrise-sunset)*100).toFixed(1)}` : `D${((now-sunrise)/(sunset-sunrise)*100).toFixed(1)}`
 
 
 		return <svg width={diameter} height={diameter} className="clock">
@@ -83,21 +95,25 @@ export class Clock extends React.Component {
 			<circle cx={center} cy={center} r={radius} className="clock__back" stroke="black" strokeWidth={stroke} fill="white" />
 			<path d={`M${center} ${center} L${rotateX(0, sunrise)} ${rotateY(0, sunrise)} A${radius} ${radius} 0 0 0 ${rotateX(0, sunset)} ${rotateY(0, sunset)} Z`} className="clock__night" stroke="none" fill="black" />
 			
-			<line x1={rotateX(0, sunrise)} y1={rotateY(0, sunrise)} x2={rotateX(outer, sunrise)} y2={rotateY(outer, sunrise)} className="clock__line" stroke="black" strokeWidth={stroke} />
 			<text x={rotateX(outer+font/4, sunrise)} y={rotateY(outer+font/4, sunrise)} fontSize={font} fontWeight="bold" textAnchor="end">D0</text>
+			<line x1={rotateX(0, sunrise)} y1={rotateY(0, sunrise)} x2={rotateX(outer, sunrise)} y2={rotateY(outer, sunrise)} className="clock__line" stroke="black" strokeWidth={stroke} />
+			<line x1={rotateX(0, sunset/4)} y1={rotateY(0, sunset/4)} x2={rotateX(outer/2, sunset/4)} y2={rotateY(outer/2, sunset/4)} className="clock__line" stroke="black" strokeWidth={stroke/2} />
 
-			<line x1={center} y1={0} x2={center} y2={-outer} className="clock__line" stroke="black" strokeWidth={stroke} />
-			<text x={center} y={-outer-font/4} fontSize={font} fontWeight="bold" textAnchor="middle">D50</text>
-
-			<line x1={rotateX(0, sunset)} y1={rotateY(0, sunset)} x2={rotateX(outer, sunset)} y2={rotateY(outer, sunset)} className="clock__line" stroke="black" strokeWidth={stroke} />
+			<text x={rotateX(outer+font/4, sunset/2)} y={rotateY(outer+font/4, sunset/2)} fontSize={font} fontWeight="bold" textAnchor="middle">D50</text>
+			<line x1={rotateX(0, sunset/2)} y1={rotateY(0, sunset/2)} x2={rotateX(outer, sunset/2)} y2={rotateY(outer, sunset/2)} className="clock__line" stroke="black" strokeWidth={stroke} />
+			<line x1={rotateX(0, sunset/4*3)} y1={rotateY(0, sunset/4*3)} x2={rotateX(outer/2, sunset/4*3)} y2={rotateY(outer/2, sunset/4*3)} className="clock__line" stroke="black" strokeWidth={stroke/2} />
+			
 			<text x={rotateX(outer+font/4, sunset)} y={rotateY(outer+font/4, sunset)} fontSize={font} fontWeight="bold">N0</text>
+			<line x1={rotateX(0, sunset)} y1={rotateY(0, sunset)} x2={rotateX(outer, sunset)} y2={rotateY(outer, sunset)} className="clock__line" stroke="black" strokeWidth={stroke} />
+			<line x1={rotateX(0, sunset+(nextSunrise-sunset)/4)} y1={rotateY(0, sunset+(nextSunrise-sunset)/4)} x2={rotateX(outer/2, sunset+(nextSunrise-sunset)/4)} y2={rotateY(outer/2, sunset+(nextSunrise-sunset)/4)} className="clock__line" stroke="black" strokeWidth={stroke/2} />
 
-			<line x1={center} y1={diameter} x2={center} y2={diameter+outer} className="clock__line" stroke="black" strokeWidth={stroke} />	
-			<text x={center} y={diameter+outer+font} fontSize={font} fontWeight="bold" textAnchor="middle">N50</text>
+			<text x={rotateX(outer+font, sunset+(nextSunrise-sunset)/2)} y={rotateY(outer+font, sunset+(nextSunrise-sunset)/2)} fontSize={font} fontWeight="bold" textAnchor="middle">N50</text>
+			<line x1={rotateX(0, sunset+(nextSunrise-sunset)/2)} y1={rotateY(0, sunset+(nextSunrise-sunset)/2)} x2={rotateX(outer, sunset+(nextSunrise-sunset)/2)} y2={rotateY(outer, sunset+(nextSunrise-sunset)/2)} className="clock__line" stroke="black" strokeWidth={stroke} />	
+			<line x1={rotateX(0, sunset+(nextSunrise-sunset)/4*3)} y1={rotateY(0, sunset+(nextSunrise-sunset)/4*3)} x2={rotateX(outer/2, sunset+(nextSunrise-sunset)/4*3)} y2={rotateY(outer/2, sunset+(nextSunrise-sunset)/4*3)} className="clock__line" stroke="black" strokeWidth={stroke/2} />
 
 			<path d={`M${center} ${center} L${rotateX(-arrow, now-0.1)} ${rotateY(-arrow, now-0.1)} L${rotateX(0, now)} ${rotateY(0, now)} L${rotateX(-arrow, now+0.1)} ${rotateY(-arrow, now+0.1)} Z`} className="clock__hand" strokeWidth={stroke} strokeLinejoin="miter" strokeMiterlimit={5} fill="black" />
 
-			<text x={center} y={center} className="clock__now" fontSize={font*2} fontWeight="bold" textAnchor="middle">{time}</text>
+			<text x={center} y={center} className="clock__now" fontSize={font*2} fontWeight="bold" textAnchor="end">{time}</text>
 		</svg>
 	}
 }
